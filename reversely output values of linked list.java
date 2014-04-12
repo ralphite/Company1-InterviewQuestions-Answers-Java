246db7041be6b15b08eb33f62eb42d9b8fe2e560
Yadong Wen <ralph.wen@gmail.com>
2014-04-13 02:03:26 +0800

    Create reversely output values of linked list.java

---
+++ reversely output values of linked list.java
@@ -0,0 +1,26 @@
+/*
+Give a linked list, reversly output all the values of the linked list.**
+*/
+
+import java.util.Stack;
+
+public class Solution {
+	public static void reversePrint(ListNode node) {
+		Stack<Integer> stack = new Stack<Integer>();
+		while(node != null) {
+			stack.push(node.val);
+			node = node.next;
+		}
+		while(!stack.isEmpty()) {
+			System.out.println(stack.pop());
+		}
+	}
+	public static class ListNode {
+		int val;
+		ListNode next;
+		ListNode(int v) {
+			val = v;
+			next = null;
+		}
+	}
+}
